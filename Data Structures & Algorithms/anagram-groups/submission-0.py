e2d23e7336b20ca41c46ec78c06302a8741e9ae5
Anmol Karan <anmolkaranva@gmail.com>
2026-05-24 18:16:58 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        fin = defaultdict(list)
+        for s in strs:
+            charLst = [0 for i in range(26)]
+            for char in s:
+                ascii_value = ord(char) - ord('a')
+                charLst[ascii_value] +=1
+            fin[tuple(charLst)].append(s)
+        return list(fin.values())
+
