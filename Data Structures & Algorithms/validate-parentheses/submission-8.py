cd956d0ea05f4db42e3afb539976540b233d58d0
Anmol Karan <anmolkaranva@gmail.com>
2026-05-27 16:50:50 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        openers = "({["
+        closers = ")}]"
+        for c in s:
+            if c in  openers:
+                stack.append(c)
+            elif c in closers:
+                if not stack: return False
+                popped = stack.pop()
+                if popped not in openers:
+                    return False
+
+                if openers.index(popped) != closers.index(c):
+                    return False
+        if not stack:
+            return  True
+        return False
