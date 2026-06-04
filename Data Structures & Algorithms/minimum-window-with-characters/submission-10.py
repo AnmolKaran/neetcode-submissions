b81436e6ce947dabdb4cefeee30d3ecec31aa070
Anmol Karan <anmolkaranva@gmail.com>
2026-06-03 21:28:58 -0400

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.py
@@ -0,0 +1,57 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        counts = defaultdict(int)
+        l = 0
+        bestL =l
+        bestR = 0
+        realCounts = defaultdict(int)
+        for c in t:
+            realCounts[c] +=1
+        if not s or not t or len(t) > len (s):
+            return ""
+        minLength = 0
+        #bestString = deque()
+        
+        counts[s[0]] = 1
+        r = 0
+        while r < len(s):
+           
+            #counts[s[r]] +=1
+       
+            substringIncluded = True
+            for c in realCounts:
+                if counts[c] < realCounts[c]:
+                    substringIncluded = False
+                    break
+
+            while substringIncluded == False:
+                
+                r +=1
+                if r >= len(s):
+                    return  s[bestL:bestR+1] if minLength != 0 else ""
+                counts[s[r]] +=1
+                
+                
+                substringIncluded = True
+                for c in realCounts:
+                    if counts[c] < realCounts[c]:
+                        substringIncluded = False
+                        break
+                
+                
+            
+            if  minLength == 0 or r- l + 1 < minLength :
+                bestL = l
+                bestR = r
+                minLength = r- l + 1
+            
+            
+            counts[s[l]] -=1
+            l+=1
+            # print(counts)
+            # print()
+        return  s[bestL:bestR+1] if minLength != 0 else ""
+
+                
+                
+            
