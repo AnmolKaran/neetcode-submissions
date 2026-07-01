bb0eb88e5557fd6659f8422f7bed173539dfb5b5
Anmol Karan <anmolkaranva@gmail.com>
2026-07-01 16:47:10 -0400

    Add: generate-parentheses - submission-4

---
+++ Data Structures & Algorithms/generate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        dp = [{""}]
+        for i in range(1,n+1):
+            prevSet = dp[i-1]
+            currSet = set()
+            for s in prevSet:
+                for i in range(len(s)):
+                    currSet.add("(" + s[:i] + ")" + s[i:])
+                currSet.add("(" + s + ")")
+                #currSet.add("()" + s)
+                #currSet.add( s + "()")
+            dp.append(currSet)
+        return list(dp[-1])
+
+
+                
+        
