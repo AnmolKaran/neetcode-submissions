4039bb38a9b52ad0cf98e87b5fd5dc03b0057468
Anmol Karan <anmolkaranva@gmail.com>
2026-07-01 13:08:30 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        finArr = []
+        
+        def dfs(start):
+            if start == len(nums):
+                finArr.append(nums.copy())
+                return
+            for i in range(start,len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+
+                dfs(start+1)
+
+                nums[start], nums[i] = nums[i], nums[start]
+
+        dfs(0)
+
+        return finArr
+        
